2b05e6f37ac1623b6e4a08354dd12ddff333ea22
AlanMandriaza <alan.mandriaza@gmail.com>
2023-04-13 04:10:23 +0000

    eror solucionado

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,36 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from enum import Enum
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
+import jwt
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+    first_name = db.Column(db.String(50), unique=False, nullable=True)
+    last_name = db.Column(db.String(50), unique=False, nullable=True)
+    date_of_birth = db.Column(db.Date(), unique=False, nullable=True)
+    address = db.Column(db.String(120), unique=False, nullable=True)
+    city = db.Column(db.String(80), unique=False, nullable=True)
+    country = db.Column(db.String(80), unique=False, nullable=True)
+    phone_number = db.Column(db.String(20), unique=False, nullable=True)
+    avatar = db.Column(db.String(120), unique=False, nullable=True)
+    roles = db.relationship('Role', secondary='user_roles')
+
+    def __init__(self, email, password, is_active=True, **kwargs):
+        self.email = email
+        self.is_active = is_active
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+        super(User, self).__init__(**kwargs)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +39,127 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "date_of_birth": self.date_of_birth.strftime('%Y-%m-%d') if self.date_of_birth else None,
+            "address": self.address,
+            "city": self.city,
+            "country": self.country,
+            "phone_number": self.phone_number,
+            "avatar": self.avatar
         }
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Role {self.name}>'
+
+class UserRole(db.Model):
+    __tablename__ = 'user_roles'
+    user_id = db.Column(db.Integer, ForeignKey('users.id'), primary_key=True)
+    role_id = db.Column(db.Integer, ForeignKey('roles.id'), primary_key=True)
+
+    def __repr__(self):
+        return f'<UserRole {self.user_id}-{self.role_id}>'
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'<Category {self.name}>'
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String(500), nullable=False)
+    category_id = db.Column(db.Integer, ForeignKey('categories.id'))
+    category = db.relationship('Category', backref=db.backref('products', lazy=True))
+    quantity = db.Column(db.Integer, nullable=False, default=0)
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+            "image": self.image,
+            "category": self.category.name,
+            "quantity": self.quantity
+        }
+
+
+class PaymentItem(db.Model):
+    __tablename__ = 'payment_items'
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, ForeignKey('products.id'))
+    product = db.relationship('Product')
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<PaymentItem {self.id}>'
+
+class Payment(db.Model):
+    __tablename__ = 'payments'
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Payment {self.id}>'
+
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', backref=db.backref('orders', lazy=True))
+    payment_id = db.Column(db.Integer, db.ForeignKey('payments.id'))
+    payment = db.relationship('Payment', backref=db.backref('orders', lazy=True))
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Order {self.id}>'
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', backref=db.backref('reviews', lazy=True))
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    product = db.relationship('Product', backref=db.backref('reviews', lazy=True))
+    comment = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Review {self.id}>'
+
+class Cart(db.Model):
+    __tablename__ = 'carts'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship('User', backref='carts')
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    items = db.relationship('CartItem', backref='cart', lazy=True)
+
+class CartItem(db.Model):
+    __tablename__ = 'cart_items'
+    id = db.Column(db.Integer, primary_key=True)
+    cart_id = db.Column(db.Integer, db.ForeignKey('carts.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    product = db.relationship('Product')
+
+    def __repr__(self):
+        return f'<CartItem {self.id}>'
--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,64 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# Signup endpoint
+@api.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+    first_name = data.get('first_name')
+    last_name = data.get('last_name')
+    date_of_birth = data.get('date_of_birth')
+    address = data.get('address')
+    city = data.get('city')
+    country = data.get('country')
+    phone_number = data.get('phone_number')
+    avatar = data.get('avatar')
+
+    if not email or not password:
+        raise APIException('Email and password are required', status_code=400)
+
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        raise APIException('User already exists', status_code=409)
+
+    new_user = User(email=email, password=password, is_active=True, first_name=first_name, last_name=last_name, date_of_birth=date_of_birth, address=address, city=city, country=country, phone_number=phone_number, avatar=avatar)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully'}), 201
+
+
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        raise APIException('Could not verify', status_code=401)
+
+    user = User.query.filter_by(email=auth.username).first()
+
+    if not user:
+        raise APIException('Could not verify', status_code=401, headers={'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    if not user.is_active:
+        raise APIException('User is inactive', status_code=401)
+
+    if not bcrypt.check_password_hash(user.password, auth.password):
+        raise APIException('Could not verify', status_code=401, headers={'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    access_token = jwt.encode({'id': user.id, 'email': user.email}, app.config['JWT_SECRET_KEY'])
+    return jsonify({'access_token': access_token.decode('UTF-8')}), 200
+
+
+@api.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    serialized_users = [user.serialize() for user in users]
+    return jsonify(serialized_users), 200
+
+
